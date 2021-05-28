7f185bae409ac13b34e2ff8bcee0f3617242ff4e
RangoDisco <maxime-dias_student2021@wilder.school>
2021-05-28 15:56:07 +0200

    various fixes

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -6,30 +6,36 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./result.component.scss']
 })
 export class ResultComponent implements OnInit {
+
+  // Show reçu par le parent
   @Input()
   randomShow;
+
   // Tableau des genres fourni par le parent
   @Input()
   genresFromShow: [];
+
   // Tableau de personnage fourni par le parent
   @Input()
   charactersList: [];
 
+  // Boolean responsable de l'affichage de la boite modale
   showModal: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    // Display modal box if there is a random show
     if (this.randomShow) {
       this.showModal = true;
-
       console.log(this.randomShow)
     }
 
   }
 
+  // Closes modal box when clicking outside it
   closeModal() {
     this.showModal = false;
-    console.log(this.showModal)
   }
 
 
